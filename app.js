0018d458eca221d76da202b71df33fe62d82abba
stavroslagos-lgtm <stavroslagos@gmail.com>
2026-03-15 13:29:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// --- SPEED ---
 function updateSpeed(position) {
     let speed = position.coords.speed; // m/s
     if (speed === null) speed = 0;
@@ -5,4 +6,35 @@ function updateSpeed(position) {
     document.getElementById("speed").innerText = kmh + " km/h";
 }
 
-navigator.geolocation.watchPosition(updateSpeed);
+// --- MAP ---
+let map = L.map('map').setView([35.3387, 25.1442], 13); // Κρήτη ως αρχικό σημείο
+
+// Tile layer (χάρτης)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19
+}).addTo(map);
+
+// Marker για το αυτοκίνητο
+let marker = L.marker([35.3387, 25.1442]).addTo(map);
+
+// Παρακολούθηση GPS
+navigator.geolocation.watchPosition(function(position) {
+    updateSpeed(position);
+
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+
+    // Μετακίνηση marker
+    marker.setLatLng([lat, lon]);
+
+    // Follow mode
+    map.setView([lat, lon]);
+}, 
+function(error) {
+    console.log("GPS error:", error);
+}, 
+{
+    enableHighAccuracy: true,
+    maximumAge: 0
+});
+
